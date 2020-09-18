7c50c65d9b559a890f7820ccda1f3b2d28037fb9
Mirko Colageo <mirko.colageo@applaudart.com>
2020-09-18 16:33:24 +0200

    added permission to s3

--- iam.tf
+++ iam.tf
@@ -20,3 +20,28 @@ resource "aws_iam_role_policy_attachment" "lambda_access_to_write_logs" {
   role       = aws_iam_role.lambda.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
 }
+
+resource "aws_iam_policy" "lambda-edge-s3-policy" {
+  name        = "lambda-edge-s3-policy"
+  description = "Policy for s3:getObject lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:getObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "image-service-role-policy-attach" {
+  role       = aws_iam_role.lambda.name
+  policy_arn = aws_iam_policy.lambda-edge-s3-policy.arn
+}
