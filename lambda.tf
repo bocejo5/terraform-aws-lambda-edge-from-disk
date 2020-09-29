5b1e819eb70060f2cad9c64a62261d560cf60e76
Mirko Colageo <mirko.colageo@applaudart.com>
2020-09-29 17:22:41 +0200

    tolto iam file

--- lambda.tf
+++ lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "lambda" {
   filename         = local.lambda_archive
   function_name    = local.name
-  role             = aws_iam_role.lambda.arn
+  role             = "${aws_iam_role.Lambda-edge-image-service-${var.environment}.arn}"
   handler          = local.handler_name
   source_code_hash = filebase64sha256(local.lambda_archive)
   runtime          = local.runtime
